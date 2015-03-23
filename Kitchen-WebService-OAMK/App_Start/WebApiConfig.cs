fa9682d72d90b56251e54864113e6971592d1f57
Bernd Verhofstadt <bernd.verhofstadt@student.ap.be>
2015-03-23 15:43:57 +0200

    Added Project files

---
+++ Kitchen-WebService-OAMK/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Kitchen_WebService_OAMK
+{
+    // Configuration of WEB API with Entity Framework
+    // http://www.asp.net/web-api/overview/data/using-web-api-with-entity-framework
+
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Kitchen-WebService-OAMK/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kitchen_WebService_OAMK.WebApiApplication" Language="C#" %>
