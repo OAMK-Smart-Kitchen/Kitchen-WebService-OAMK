530c715b3f0e9a4ccd4b5f0570eca70fae04dba6
Bernd Verhofstadt <bernd.verhofstadt@student.ap.be>
2015-03-31 19:35:10 +0300

    Json enabled

--- WebServiceSmartKitchen/WebServiceSmartKitchen/App_Start/WebApiConfig.cs
+++ WebServiceSmartKitchen/WebServiceSmartKitchen/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace WebServiceSmartKitchen
@@ -10,7 +11,9 @@ namespace WebServiceSmartKitchen
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            
+            // Web API JSON enabled
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
             // Web API routes
             config.MapHttpAttributeRoutes();
 
