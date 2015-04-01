426f63cbb4ca413f098a8d47562c892af2b36882
Bernd Verhofstadt <bernd.verhofstadt@student.ap.be>
2015-04-01 12:11:16 +0300

    Deleted Old project, changed dir for new one

--- SmartKitchenDB/ShoppingBags.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[ShoppingBags] (
-    [Id]        INT            IDENTITY (1, 1) NOT NULL,
-    [name]      NVARCHAR (MAX) NOT NULL,
-    [createdOn] NVARCHAR (MAX) NOT NULL,
-    CONSTRAINT [PK_dbo.ShoppingBags] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
