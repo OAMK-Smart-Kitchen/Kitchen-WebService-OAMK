f791c3f257b17627b520f03d7aaea2b8bc2cd967
Bernd Verhofstadt <bernd.verhofstadt@student.ap.be>
2015-04-17 14:35:15 +0300

    Override savechanges

--- WebServiceSmartKitchen/Models/Model.Context.cs
+++ WebServiceSmartKitchen/Models/Model.Context.cs
@@ -10,8 +10,11 @@
 namespace WebServiceSmartKitchen.Models
 {
     using System;
+    using System.Linq;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
+    using System.Data.Entity.Validation;
+    using System.Threading.Tasks;
     
     public partial class ModelContainer : DbContext
     {
@@ -19,6 +22,33 @@ namespace WebServiceSmartKitchen.Models
             : base("name=ModelContainer")
         {
         }
+
+        // To override SaveChanges
+        // http://devillers.nl/improving-dbentityvalidationexception/
+        public override async Task<int> SaveChangesAsync()
+        {
+            try
+            {
+                return await base.SaveChangesAsync();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                // Retrieve the error messages as a list of strings.
+                var errorMessages = ex.EntityValidationErrors
+                        .SelectMany(x => x.ValidationErrors)
+                        .Select(x => x.ErrorMessage);
+
+                // Join the list to a single string.
+                var fullErrorMessage = string.Join("; ", errorMessages);
+
+                // Combine the original exception message with the new one.
+                var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
+
+                // Throw a new DbEntityValidationException with the improved exception message.
+                throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
+            }
+        }
+    
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
